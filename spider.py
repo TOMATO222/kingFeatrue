826e1bad99914cd92d4843908235a0d437f7171d
tanyuyao <tyy9992526@gmail.com>
2023-10-17 17:41:32 +0800

    完成爬取十二本纪的代码

--- spider.py
+++ spider.py
@@ -13,6 +13,8 @@ import os
 
 import urllib.request
 import urllib.parse
+import time
+import random
 
 
 def mkdir(path):
@@ -54,97 +56,120 @@ def circlePage(i):  # 遍历每一页
     return data
 
 
-class ihchina:
+class Historian:
     # # 定义要创建的目录
     # mkpath = "E:/spidertxt/MyModel"
     # mkdir(mkpath)
 
-    def __init__(self):
-        pass
+    def __init__(self, url, path, headers):
+        self.url = url
+        self.path = path
+        self.links = []
+        self.headers = headers
 
-    def run(self):
-        print("################################ ihchina ################################")
-        # 非遗博物馆
-        self._get_ihchina_root_info()
-        self._parser_ihchina_info()
+    def getList(self):
+        print("**********获取指定内容的网址**********")
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            content = soup.find(name="div", attrs={"class": "sons"})
+            twelve_list = content.find(name="div", attrs={"class": "bookcont"})
+            list = twelve_list.find_all('a')
+            pre_link = 'https://shiwens.com'
+            for single in list:
+                link = pre_link + single.get('href')
+                self.links.append(link)
 
-    def _get_ihchina_root_info(self):
-        with open("E:/spidertxt/MyModel/MS/ihchina_link.txt", "wt+") as f:
-            count = 0
-            url = "http://www.ihchina.cn/Article/Index/getProject.html?"
-            for i in range(1, 50):
-                data = circlePage(i)
-                s = requests.session()
-                r = s.get(url, params=data)
-                # self.soup = BeautifulSoup(r.text, "html.parser")
-                f_data = r.json()
-                f_data = f_data['list']
-                # data = data['id']
-                for j in range(len(f_data)):
-                    count += 1
-                    # print('http://www.ihchina.cn/project_details/' + f_data[j]['id'] + '/')
-                    temp = 'http://www.ihchina.cn/project_details/' + f_data[j]['id'] + '/'
-                    f.write(temp)
-                    f.write('\n')
-                    print(count)
-            f.close()
 
-    def _parser_ihchina_info(self):
+    def getContent(self):
+        print("**********开始获取每个网址下的具体内容**********")
         # 写入csv文件
-        csv_file = open("E:/spidertxt/MyModel/MS/ihchina_info.csv", "a+", encoding='utf-8', newline='')
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+            print(self.path + " build success")
+        else:
+            print(self.path + " already exist")
+        filePath = os.path.join(self.path, 'twelveContent.csv')
+        csv_file = open(filePath, "a+", encoding='utf-8', newline='')
         try:
             csv_writer = csv.writer(csv_file, delimiter=",")
-            header = ["NAME", "INFO"]  # csv文件头
+            header = ["NAME", "CONT"]  # csv文件头
             csv_writer.writerow(header)
         finally:
             csv_file.close()
 
-        count_all = 0
-        count_delete = 0
-        ctjy_name = ''  # 存储传统技艺名称(去重存入txt）
-        ctjy_name2 = [] # 不去重直接存入csv
-        ctjy_info = []  # 存储传统技艺内容
-        with open("E:/spidertxt/MyModel/MS/ihchina_link.txt", "r") as f:
-            for line in f.readlines():
-                line = line.strip()
-                count_all += 1
-                # 尝试爬取
-                try:
-                    r = requests.get(line, timeout=30)
-                    r.raise_for_status()
-                    r.encoding = r.apparent_encoding
-                except:
-                    print('Error')
-                    break
-                self.soup = BeautifulSoup(r.text, "html.parser")
-                # 开始解析内容
-                content = self.soup.find(name="div", attrs={"class": "x-container"})
-                name = content.find(name="div", attrs={"class": "h30"})
-                name = name.get_text()  # 传统技艺名称
-                ctjy_name2.append(name)
-                print(name)
-                if name not in ctjy_name:
-                    ctjy_name = ctjy_name + name + '\n'
-                    count_delete += 1
-                info = content.find(name="div", attrs={"class": "text"})
-                info = info.find(name="div", attrs={"class": "p"})
-                info = info.get_text()
-                print(info)
-                print("--------------------------------------------------")
-                ctjy_info.append(info)
-            f.close()
-        print("----------------------------遍历完成----------------------------")
-        print("所有技艺", count_all)
-        print("技艺名去重", count_delete)
-        with open("E:/spidertxt/MyModel/MS/ihchina_name.txt", "wt+", encoding='utf-8') as f:
-            f.write(ctjy_name)
-        print("---------------传统技艺名称（去重）存储成功---------------")
-        csv_file = open("E:/spidertxt/MyModel/MS/ihchina_info.csv", "a+", encoding='utf-8', newline='')
+        titleList = []
+        contentList = []
+        for link in self.links:
+            response = requests.get(link)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, "html.parser")
+                loc = soup.find(name="div", attrs={"class": "main3"}).find(name="div", attrs={"class": "cont"})
+                # title
+                title = loc.find('b').get_text()
+                print(title)
+                titleList.append(title)
+                # content
+                content = ''
+                lines = loc.find(name="div", attrs={"class": "contson"})
+                lines = lines.find_all('p')
+                for line in lines:
+                    content += line.get_text() + '\n'
+                contentList.append(content)
+            else:
+                print("error")
+            # # 设置一个随机的延时，例如在1到3秒之间
+            # delay = random.uniform(1, 3)
+            # time.sleep(delay)
+
+        print("**开始写入文件**")
+        csv_file = open(filePath, "a+", encoding='utf-8', newline='')
         try:
             csv_writer = csv.writer(csv_file, delimiter=",")
-            csv_writer.writerows(zip(ctjy_name2, ctjy_info))
+            csv_writer.writerows(zip(titleList, contentList))
         finally:
             csv_file.close()
-            print("---------------传统技艺信息（未去重）存储成功---------------")
+
+    def forTest(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
+            loc = soup.find(name="div", attrs={"class": "main3"}).find(name="div", attrs={"class": "cont"})
+            # title
+            title = loc.find('b').get_text()
+            print(title)
+            # content
+            content = ''
+            lines = loc.find(name="div", attrs={"class": "contson"})
+            lines = lines.find_all('p')
+            for line in lines:
+                content += line.get_text() + '\n'
+                # content += line.get_text()
+                # content += '\n'
+            with open('/Users/tanyuyao/Documents/PaperDocument/OriginalContent/test.txt', 'w', encoding='utf-8') as f:
+                f.write(content)
 
 
+
+if __name__ == '__main__':
+    # 自定义请求头
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Referer': 'https://example.com',
+        'Accept-Language': 'en-US,en;q=0.5',
+    }
+
+    url = 'https://shiwens.com/book_361.html'
+
+    folder_path = os.path.expanduser('~/Documents/PaperDocument/OriginalContent')
+
+    spider_twlve = Historian(url, folder_path,headers)
+
+    # 获取十二本纪的网页地址
+    spider_twlve.getList()
+    for link in spider_twlve.links:
+        print(link)
+    # 获取十二本纪内容并存储
+    spider_twlve.getContent()
+
+    # spider_twlve.forTest("https://shiwens.com/bookv_23578.html")
